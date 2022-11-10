d8e98abb6985bf2da75987871a282d65c06c8c0b
RolandoMaradiaga <rolandomaradiaga23@gmail.com>
2022-11-09 20:03:08 -0600

    Hashing level of security added with md5

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require("mongoose")
 const app = express();
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
 
 //console.log(process.env.API_KEY);ex. tap into the env file to get the secret key
 
@@ -22,8 +22,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-//always add the user plugin befor the modal
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ["password"]}); //encryptedFields encrypts specific fields
 
 const User = new mongoose.model("User", userSchema);
 
@@ -42,7 +40,7 @@ app.get("/register", (req,res) =>{
 app.post("/register", (req,res) =>{
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   })
 
   newUser.save((err)=>{
@@ -56,7 +54,7 @@ app.post("/register", (req,res) =>{
 
 app.post("/login", (req,res) => {
   const userName = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email:userName},(err,foundUser)=>{
     if(!err){
